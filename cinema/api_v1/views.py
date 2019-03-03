3d406ce2e1b4f503c515f54bd86333670368ec31
ваше имя < >
2019-03-04 01:20:58 +0600

    Added models

--- cinema/api_v1/views.py
+++ cinema/api_v1/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from webapp.models import Movie, Category, Hall, Seat, Show
+from rest_framework import viewsets
+from api_v1.serializers import MovieSerializer, CategorySerializer, HallSerializer, SeatSerializer, ShowSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().order_by('-release_date')
+    serializer_class = MovieSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+
+class HallViewSet(viewsets.ModelViewSet):
+    queryset = Hall.objects.all().order_by('name')
+    serializer_class = HallSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all().order_by('seat')
+    serializer_class = SeatSerializer
+
+class ShowViewSet(viewsets.ModelViewSet):
+    queryset = Show.objects.all().order_by('movie')
+    serializer_class = ShowSerializer
--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+    release_date = models.DateField()
+    finish_date = models.DateField(null=True, blank=True)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+class Hall(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Seat(models.Model):
+    hall = models.ForeignKey(Hall, on_delete=models.PROTECT)
+    row = models.DecimalField(max_digits=2, decimal_places=0)
+    seat = models.DecimalField(max_digits=3, decimal_places=0)
+
+    def __str__(self):
+        return self.seat
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    hall = models.ForeignKey(Hall, on_delete=models.PROTECT)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.movie

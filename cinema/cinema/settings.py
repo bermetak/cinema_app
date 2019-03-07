58cd29840c9a98a03b24e8068b997be0e7213574
ваше имя < >
2019-03-07 19:31:05 +0600

    Installed django-cors-headers

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'webapp',
     'api_v1',
 ]
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,6 @@ MEDIA_URL = '/uploads/'
 
 
 RESERVATION_CODE_LENGTH = 6
+
+
+CORS_ORIGIN_ALLOW_ALL = True

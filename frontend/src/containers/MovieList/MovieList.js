42063a51b9c4c18f6cc8ec733e8b5637eac491c5
ваше имя < >
2019-03-14 00:19:20 +0600

    Added MovieList

---
+++ frontend/src/containers/MovieList/MovieList.js
@@ -0,0 +1,35 @@
+import React, {Fragment, Component} from 'react'
+import {MOVIES_URL} from "../../urls";
+import MovieCard from "../../components/MovieCard/MovieCard";
+import {NavLink} from "react-router-dom";
+import axios from 'axios';
+
+
+class MovieList extends Component {
+    state = {
+        movies: [],
+    };
+
+    componentDidMount() {
+        axios.get(MOVIES_URL)
+            .then(response => {console.log(response.data); return response.data;})
+            .then(movies => this.setState({movies}))
+            .catch(error => console.log(error));
+    }
+
+    render() {
+        return <Fragment>
+            <p><NavLink to='/movies/add'>Добавить фильм</NavLink></p>
+            <div className='row'>
+                {this.state.movies.map(movie => {
+                    return <div className='col-xs-12 col-sm-6 col-lg-4 mt-3'  key={movie.id}>
+                        <MovieCard movie={movie}/>
+                    </div>
+                })}
+            </div>
+        </Fragment>
+    }
+}
+
+
+export default MovieList;
